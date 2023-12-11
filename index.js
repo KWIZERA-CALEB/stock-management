1fb734693f025d44f3fa44b7e7644e5ad23f8092
Kwizera Caleb <kwizeracaleb91@gmail.com>
2023-12-11 09:55:41 +0200

    Version 1 restored

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ let myVar = "Hello World";
 console.log(myVar);
 
 window.alert();
-
-var $$$ = "Am a string";
-const price = 300;
-const price2 = 300;
-const all = price + price2;
-console.log(all);
